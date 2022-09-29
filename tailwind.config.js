0eb4331424b904997e03bcbe4ac00e1227436b5e
Michael Hulet <michael@hulet.eu>
2022-09-29 23:35:28 +0200

    Add hubs on the homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,6 +39,7 @@ module.exports = {
       },
       fontFamily: {
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        serif: ['Vesper Libre', ...defaultTheme.fontFamily.serif]
       },
     },
   },
